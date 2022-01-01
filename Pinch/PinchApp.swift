4c33c97138572fd0c787a5977b73d0009669e2ce
uzairriaz <uzairriaz.pk@gmail.com>
2022-01-01 15:40:18 +0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Uzair Riaz on 01/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
